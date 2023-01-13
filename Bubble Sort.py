24af87136c59c083629c3254cec5da2bf96c9ed7
Kibreab Aklilu <117360366+Keba777@users.noreply.github.com>
2023-01-13 22:30:51 +0300

    Update Bubble Sort.py
    Hackrrank Problem.

--- Bubble Sort.py
+++ Bubble Sort.py
@@ -1,13 +1,33 @@
-totalSwaps = 0
-for i in range(0, len(a)-1):
-	numSwaps = 0
-	for j in range(0,len(a)-1):
-		if a[j] > a[j+1]:
-			a[j],a[j+1] = a[j+1], a[j]
-			numSwaps += 1
-			totalSwaps +=1
-	if numSwaps == 0:
-		break
-print("Array is sorted in {} swaps.".format(totalSwaps))
-print("First Element: {}".format(a[0]))
-print("Last Element: {}".format(a[len(a)-1]))
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countSwaps' function below.
+#
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def countSwaps(a):
+    # Write your code here
+    count = 0
+    for i in range(1, len(a)):
+        for j in range(len(a) - 1):
+            if a[j] > a[j + 1]:
+                a[j], a[j + 1] = a[j + 1], a[j]
+                count += 1
+    
+    print("Array is sorted in", count, "swaps.")
+    print("First Element:", a[0])
+    print("Last Element:", a[-1])
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    countSwaps(a)
