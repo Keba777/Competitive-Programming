b4eb3769f4ace76c796dd4cb95f414c785f1bda3
Kibreab Aklilu <117360366+Keba777@users.noreply.github.com>
2023-02-08 11:17:43 +0300

    Create Minimum Increment to Make Array Unique.py
    LeetCode Problem.

---
+++ Minimum Increment to Make Array Unique.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minIncrementForUnique(self, nums: List[int]) -> int:
+        increments = 0
+        last_used = -1
+
+        for num in sorted(nums):
+            if num <= last_used:
+                increments += last_used + 1 - num
+                last_used += 1
+            else:
+                last_used = num
+        return increments
